6552108ee652020aa0a781ccaa5f0c43f86a3799
Fernando Gallegos <fernando.gallegos@mtnet.com.mx>
2020-02-28 17:37:43 -0700

    Cambios minomos a los archivos cache

--- testus.py
+++ testus.py
@@ -2,12 +2,16 @@
 from lxml import etree, objectify
 from io import StringIO, BytesIO
 
+from datetime import datetime, timedelta
+
 import hashlib
 import base64
 
 #----------------------------------------------------------------------
     
 """
+https://developers.sw.com.mx/knowledge-base/consumo-webservice-descarga-masiva-sat/
+solo metadata
 """
 
 #----------------------------------------------------------------------
@@ -43,9 +47,9 @@ if __name__ == "__main__":
     
     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-    #imprimirArbol(root)
+    # imprimirArbol(root)
     
-    mod = root.findall(".//{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp")
+    mod = root.find(".//{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp")
     """
     print(mod[0].text)
     mod[0].text = "GG IZ"
@@ -53,25 +57,36 @@ if __name__ == "__main__":
     """
     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
-    txt = etree.tostring(mod[0])
+    # txt = etree.tostring(mod[0])
     print("------------")
-    etree.cleanup_namespaces(mod[0],)
+    # etree.cleanup_namespaces(mod[0],)
+    #print(mod)
     print("-------<>-------")
-    lrm = etree.XML('<u:Timestamp xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" u:Id="_0"><u:Created>2019-01-09T16:48:50.000Z</u:Created><u:Expires>2019-01-09T16:53:50.000Z</u:Expires></u:Timestamp>')
 
-    for child in lrm:
-        print(child.tag)
-
-    print(etree.tostring(lrm, pretty_print=False))
-    print("-------<>-------")
-    print(str(txt))
+    # print(etree.tostring(lrm, pretty_print=False))
+ 
+    #print(etree.tostring(mod))
     # txt = str(txt, 'utf-8')
     # mod[0].xpath("//{http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd}Timestamp[@xmlns:o=\'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/\'")
+    
+    # info = mod.nsmap
 
-    preValue = hashlib.sha1()
-    preValue.update(etree.tostring(mod[0]))
-    print(preValue.digest())
-    print(base64.b64encode(preValue.digest()))
+
+    # mod.nsmap = info
+    # https://lxml.de/api/lxml.etree._Element-class.html
+    dataToDigest = etree.XML('<u:Timestamp xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" u:Id="_0"><u:Created>2019-01-09T16:48:50.000Z</u:Created><u:Expires>2019-01-09T16:53:50.000Z</u:Expires></u:Timestamp>')
+
+    createdFieldDigestData = dataToDigest.find("{*}Created")
+    expiresFieldDigestData = dataToDigest.find("{*}Expires")
+    
+    createdFieldDigestData.text = "2019-01-09T16:48:50.000Z"
+    expiresFieldDigestData.text ="2019-01-09T16:53:50.000Z"
+    # Parte del procedimeitno qu ese va a llevar
+    print("-------<>-------")
     
 
+    preValue = hashlib.sha1()
+    preValue.update(etree.tostring(dataToDigest))
+    print(preValue.digest())
+    print(base64.b64encode(preValue.digest()))
 
