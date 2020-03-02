bcd80a50ba8e8577675d37cd757d68278293a672
Fernando Gallegos <fernando.gallegos@mtnet.com.mx>
2020-03-02 11:17:14 -0700

    [ADD]master manejo de errores minimo

--- easySAT.py
+++ easySAT.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import cfdiclient
+
 
 class Sat_pet:
     """An easy way to manage and download Metadata and xml reports for SAT"""
@@ -18,7 +19,7 @@ class Sat_pet:
 
     packages_to_download = False
 
-    def __init__(self, RFC, passFiel, fiel_cer, fiel_key):
+    def __init__(self, RFC, passFiel, fiel_cer, fiel_key, backup=False):
         cer_der = open(fiel_cer, 'rb').read()
         key_der = open(fiel_key, 'rb').read()
 
@@ -26,31 +27,45 @@ class Sat_pet:
         self.RFC = RFC
         self.fiel = cfdiclient.Fiel(cer_der, key_der, passFiel)
 
+        if backup:
+            self.RFC = backup['RFC']
+            self.token = backup['token']
+
     def make_auth(self):
         auth = cfdiclient.autenticacion.Autenticacion(self.fiel)
 
         self.token = auth.obtener_token()
 
-    def create_dates(self, start_year, start_month, start_day, end_year, end_month, end_day):
-        self.dates = {'start': datetime(int(start_year), int(start_month), int(start_day)),
-                      'end': datetime(int(end_year), int(end_month), int(end_day))}
+        if self.token != '':
+            return True
 
-    def make_download_petition(self, RFC_emissor = False, RFC_receiver = None, only_metadata=True):
+        return False
+
+    def create_dates(self, start_year, start_month, start_day, end_year, end_month, end_day):
+        try:
+            self.dates = {'start': datetime(int(start_year), int(start_month), int(start_day)),
+                          'end': datetime(int(end_year), int(end_month), int(end_day))}
+        except:
+            self.dates = False
+            return False
+
+    def make_download_petition(self, RFC_emissor=False, RFC_receiver=None, only_metadata=True):
         if not self.dates:
             return "ERROR: to continue the app need the dates to look on index"
 
         type_of_download = 'Metadata'
 
-        if not RFC_emissor:
+        if not RFC_emissor and RFC_receiver is None:
             RFC_emissor = self.RFC
 
         if not only_metadata:
             type_of_download = 'CFDI'
-            
 
         download = cfdiclient.solicitadescarga.SolicitaDescarga(self.fiel)
 
-        result = download.solicitar_descarga(self.token, self.RFC, self.dates['start'], self.dates['end'], rfc_emisor= RFC_emissor, tipo_solicitud=type_of_download)
+        result = download.solicitar_descarga(self.token, self.RFC, self.dates['start'], self.dates['end'],
+                                             rfc_emisor=RFC_emissor, rfc_receptor=RFC_receiver,
+                                             tipo_solicitud=type_of_download)
 
         self.id_download_solicitude = result['id_solicitud']
 
@@ -63,7 +78,7 @@ class Sat_pet:
 
         self.packages_to_download = data['paquetes']
 
-        print(result)
+        print(data)
 
     def download_packages(self):
         if len(self.packages_to_download) == 0:
@@ -78,5 +93,13 @@ class Sat_pet:
 
         return data
 
+    def save_as_dic(self):
+        return {'token': self.token,
+                'RFC': self.RFC,
+                'dates': self.dates,
+                'id_download_solicitude': self.id_download_solicitude,
+                'packages_to_download': self.packages_to_download}
+
     def __str__(self):
-        return "RFC: %s\nToken: %s\nFechas: %s\nIDDescarga: %s\nCantidad de paquetes: %s" % (self.RFC, self.token, self.dates, self.id_download_solicitude, self.packages_to_download)
+        return "RFC: %s\nToken: %s\nFechas: %s\nIDDescarga: %s\nCantidad de paquetes: %s" % (
+        self.RFC, self.token, self.dates, self.id_download_solicitude, self.packages_to_download)
